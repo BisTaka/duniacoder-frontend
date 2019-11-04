00350c0e0473496c65391f2ae988c44973bcc2a6
BisTaka <bismantaka20@hotmail.com>
2019-11-04 14:35:13 +0700

    v2.0 Release with some fixing

--- assets/backup.js
+++ assets/backup.js
@@ -1,11 +1,10 @@
-$('#Hero3,#Hero4,#Hero2,#Button-kenal,#Carousel2,#ButtonLanjut2,#Sub,#Hero5').each(function () {
+$('#Hero3,#Hero4,#Hero2,#Button-kenal,#Carousel2,#ButtonLanjut2,#Sub,#Hero5,#Hero6,#Hero7,#HeroFooter').each(function () {
     $(this).hide()
 })
-$('#Intro').click(function () {
-    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: black;')
+let cuk = $('#Intro').click(function () {
+    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: white;')
     let typed = new Typed('#Sub', {
-        startDelay: 3000,
-        strings: ["Hai, Selamat Datang di Web kami!!! 🥰", "Web ini dibuat karena iseng juga tidak ada pekerjaan 🤣", 'Ingin Mengenal kami lebih lanjut? 🤔'],
+        strings: ["Hai, Selamat Datang di Web kami!!! 🥰", "Web ini dibuat karena iseng juga karena tidak ada pekerjaan 🤣", 'Ingin Mengenal kami lebih lanjut? 🤔'],
         typeSpeed: 50,
         fadeOut: true,
     });
@@ -18,7 +17,7 @@ $('#Intro').click(function () {
 })
 
 $('#Button-kenal').click(() => {
-    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: black;')
+    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: white;')
     $('#Hero').fadeOut(1000)
     $('#Hero2').fadeIn(3000)
     $(".owl-carousel").owlCarousel({
@@ -29,24 +28,49 @@ $('#Button-kenal').click(() => {
 })
 
 $('#ButtonLanjut').click(() => {
-    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: black;')
-    $('#Hero2').fadeOut(1000)
-    $('#Hero3').fadeIn(2500)
+    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: white;')
+    $('#Hero6').show(1000)
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#Hero6").offset().top
+    }, 2000)
+    $('#Hero2').hide(3000)
 })
 
-$('#ButtonCarousel').click( () => {
-    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: black;')
-    $('#Carousel2').fadeIn(2000)
-    $('#ButtonLanjut2').fadeIn(4000)
+$('#ButtonLanjut12').click( () => {
+    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: white;')
+    $('#Hero7').show(2000)
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#Hero7").offset().top
+    }, 2000)
+    $('#Hero3').hide(2000)
+    // $('#ButtonLanjut2').fadeIn(4000)
+})
+
+$('#ButtonLanjut13').click( () => {
+    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: white;')
+    $('#Hero4').fadeIn(3000)
+    $('#Hero7').fadeOut(2000)
+    // $('#ButtonLanjut2').fadeIn(4000)
 })
 
 $('#ButtonLanjut2').click(() => {
-    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: black;')
+    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: white;')
     $('#Hero3').fadeOut(1000)
     $('#Hero4').fadeIn(2500)
+})
+
+$('#ButtonLanjut0').click(() => {
+    console.log('%cDeveloped by: Bismantaka Aldila', 'color: purple; font-style: bold; background-color: white;')
+    $('#Hero6').fadeOut(1000)
+    $('#Hero3').fadeIn(2500)
 })
 
 $('#ButtonLanjut4').click(() => {
     $('#Hero4').fadeOut(1000)
     $('#Hero5').fadeIn(2500)
+    $('#HeroFooter').fadeIn(3000)
+})
+
+$('#ButtonLanjut3').click(() => {
+    location.reload();
 })
